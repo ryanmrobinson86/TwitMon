2b97ee4b6c241e0938fe22fa31354fc9e5799254
Ryan Robinson <ryan@ryanrobinson.name>
2014-10-19 23:13:36 +0000

    Initial Commit of TwitMon

---
+++ TwitMon.py
@@ -0,0 +1,41 @@
+import time
+import RPi.GPIO as GPIO
+import sys
+from twython import TwythonStreamer
+
+# Search Terms
+TERMS = '#fml'
+
+LED = 22
+
+#Twitter app keys
+CONSUMER_KEY = 'jnBo9oveigooTbIoISyGvgTQu'
+CONSUMER_SECRET = 'hwFEiykTGIab7j4Wox1b1cW8NtdB9J4IJHnn69HbEKOOdSVorY'
+ACCESS_TOKEN = '47125686-HjSO8eykmh0V7s7p6tz80ZhnBxTiqtsxQ76CvecEQ'
+ACCESS_TOKEN_SECRET = 'BiQgWbLeiuAqSZryRyTKKlASmMxsfp4XOdL0yIyiohfPZ'
+
+# Callback for Twython Streamer
+class TwitMonStreamer(TwythonStreamer):
+	def on_success(self, data):
+		if 'text' in data:
+			print data['text'].encode('utf-8')
+			print
+			GPIO.output(LED, GPIO.HIGH)
+			time.sleep(0.5)
+			GPIO.output(LED, GPIO.LOW)
+
+# Setup GPIO as output
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(LED, GPIO.OUT)
+GPIO.output(LED, GPIO.LOW)
+
+#create streamer
+try:
+	stream = TwitMonStreamer(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+	if len(sys.argv) > 1:
+		stream.statuses.filter(track=sys.argv[1])
+	else:
+		stream.statuses.filter(track=TERMS)
+except KeyboardInterrupt:
+	GPIO.cleanup()
+
